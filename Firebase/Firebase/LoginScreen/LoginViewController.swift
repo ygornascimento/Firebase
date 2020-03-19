c917b667a69cdddf6abb826a699832bf9464bbed
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-03-19 09:23:05 -0300

    Login Screen

--- Firebase/Firebase/LoginScreen/LoginViewController.swift
+++ Firebase/Firebase/LoginScreen/LoginViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let loginStackView = LoginStackView()
+    let viewHeight = UIScreen.main.bounds.size.height * 0.300
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
     }
 
+    func setupView() {
+        view.addSubview(loginStackView)
+        loginStackView.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+
+        NSLayoutConstraint.activate([
+            loginStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            loginStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            loginStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -viewHeight)
+        ])
+    }
 
 }
 
