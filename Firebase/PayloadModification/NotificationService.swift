2ebe9cd48cd9206453d42f710dc4cc2595fb2b43
Ygor Nascimento <nascimento.ygor@outlook.com>
2020-03-25 10:07:58 -0300

    Deeplinks with Notifications

---
+++ Firebase/PayloadModification/NotificationService.swift
@@ -0,0 +1,39 @@
+//
+//  NotificationService.swift
+//  PayloadModification
+//
+//  Created by Ygor Nascimento on 23/03/20.
+//  Copyright © 2020 Ygor Nascimento. All rights reserved.
+//
+
+import UserNotifications
+import FirebaseMessaging
+
+class NotificationService: UNNotificationServiceExtension, MessagingDelegate {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified by Ygor]"
+            
+            contentHandler(bestAttemptContent)
+
+            
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
